8031aa3ba82542380f883c6c728e9628b4d3ff22
Luis Perez <42252004+lpbw@users.noreply.github.com>
2022-02-23 12:47:35 -0700

    Revert "original"
    This reverts commit 92ec6b7fd2f796829369db9bac07464211e16381.

--- coneccion.php
+++
@@ -1,9 +0,0 @@
-<?
-	$enlace = mysql_connect('localhost', 'bluewol5_textron', 'Textron11!');
-	mysql_set_charset('utf8',$enlace);
-	if (!$enlace) { 
-    die('Could not connect: ' . mysqli_error($enlace)); 
-	} 
-
-	mysql_select_db("bluewol5_timpmp") or die("No pudo seleccionarse la BD.");
-?>
--- coneccion_i.php
+++
@@ -1,8 +0,0 @@
-<?
-	$enlace = mysqli_connect('localhost', 'bluewol5_textron', 'Textron11!', "bluewol5_timpmp"); //host, user, pass, bd
-	mysqli_set_charset($enlace, 'utf8');
-	if (!$enlace) { 
-    	die('Could not connect:'); 
-	} 
-
-?>
